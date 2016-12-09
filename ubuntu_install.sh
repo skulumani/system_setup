cd573414465aa3dce38bf04a46b9bcf51942866f
Shankar Kulumani SEAS-Linux <skulumani@gwmail.gwu.edu>
2016-12-09 16:20:34 -0500

    Add initial start of setup script

---
+++ ubuntu_install.sh
@@ -0,0 +1,31 @@
+# !/bin/bash
+
+# add repos
+
+# Google Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+# sublime text 3
+
+# drive client
+
+# download latest Texlive and update
+
+sudo dpkg -i ./google-chrome*.deb
+
+sudo apt-get update
+
+sudo apt-get install google-chrome-stable
+
+
+# basic update
+sudo apt-get -y --force-yes update
+sudo apt-get -y --force-yes upgrade
+
+
+# install all the apps
+
+# Configure necessary git repos
+
+# copy dot files from repo
+
