13f7f131f8fbffe1e031c301db7536493a741ebe
Shankar Kulumani <shanks.k@gmail.com>
2017-04-19 19:56:30 -0400

    Update linux install script

--- linux_setup.sh
+++ linux_setup.sh
@@ -1,17 +1,47 @@
 #!/usr/bin/env bash
+# trap functions to delete temporary folder on exit
+# find directory of script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# create a temp directory inside $DIR
+WORK_DIR='mktemp -d -p "$DIR"'
+
+# make sure tmp dir was actually created
+if [[ ! "$WORK_DIR" || -d "$WORK_DIR" ]]; then
+    echo "Could not create temp directory"
+    exit 1
+fi
+
+# delete temp dir
+cleanup () {
+    rm -rf "$WORK_DIR"
+    echo "Deleted temp working directory: $WORK_DIR"
+}
+
+# register cleanup function to be called on exit
+trap cleanup EXIT
+########################################################################
 # list of packages to download for linux
 
 # anaconda version
 anaconda_version="4.3.1"
 anaconda_hash="4447b93d2c779201e5fb50cfc45de0ec96c3804e7ad0fe201ab6b99f73e90302"
 
-brews=(
-    ctags
+# go lang source
+go_version="1.8.1"
+go_hash="a579ab19d5237e263254f1eac5352efcf1d70b9dacadb6d6bb12b0911ede8994"
+
+apt_packages=(
+    exuberant-ctags
     git
-    go
     myrepos
     tmux
     zsh
+    vim-gnome
+    zathura
+    zathura-dev
+    xdotool
+    redshift-gtk
 )
 
 python_packages=(
@@ -34,7 +64,7 @@ prompt () {
     done
 }
 command_exists () {
-command -v "$1" >/dev/null 2>&1 ;
+    command -v "$1" >/dev/null 2>&1 ;
 }
 
 install_pips () {
@@ -47,25 +77,25 @@ install_pips () {
     done    
 }
 
-install_anaconda () {
-    if [ ! -d "$HOME/anaconda3" ]; then
-        echo "Anaconda is not installed"
-        prompt "Download Anaconda install script" "wget https://repo.continuum.io/archive/Anaconda3-${anconda_version}-Linux-x86_64.sh -O $HOME/anaconda.sh"
-        
-        if [ ! sha256sum -c <<< "$anaconda_hash anaconda.sh" ]; then
-            echo "Hash does not match. Aborting!"
-            exit 1
-        else
-            echo "Hash match. Installing"
-            prompt "Install Anaconda" "bash $HOME/anaconda.sh -b -p $HOME/anaconda3"
-        fi
+install_packages () {
+    cmd="sudo apt-get -y install"
+    shift
+    for pkg in "${apt_packages[@]}";
+    do
+        exec="$cmd ${pkg}"
+        prompt "Execute: $exec" "${exec}"
+    done    
 }
-install_chrome () {
-sudo dpkg -i google-chrome*.deb
 
-
-}
 ######################################## Functions #############################################
+# install graphics drivers
+
+echo "We're going to update and install a bunch of stuff"
+sudo apt-get -y update
+prompt "Add NVIDIA driver repo" "sudo add-apt-repository ppa:graphics-drivers/ppa"
+
+# download packages
+prompt "Install packages" "install_packages"
 # check and then install chrome
 if command_exists google-chrome; then
     echo "Google Chrome already installed"
@@ -76,12 +106,42 @@ else
 fi
 
 # install anaconda
-echo "We're going to install Anaconda using Homebrew"
-prompt "Install Anaconda" "brew cask install anaconda"
+if [[ ! -d "$HOME/anaconda3" ]]; then
+    echo "Anaconda is not installed"
+
+    prompt "Download Anaconda install script" "wget https://repo.continuum.io/archive/Anaconda3-${anconda_version}-Linux-x86_64.sh -O $WORK_DIR/anaconda.sh"
+
+    if [ ! sha256sum -c <<< "${anaconda_hash} ${WORK_DIR}/anaconda.sh" ]; then
+        echo "Hash does not match. Aborting!"
+        exit 1
+    else
+        echo "Hash match. Installing Anaconda"
+        prompt "Install Anaconda" "bash ${WORK_DIR}/anaconda.sh -b -p ${HOME}/anaconda3"
+    fi
+else
+    echo "Anaconda is already installed"
+fi
+
+# install the go language
+if [[ ! -d "/usr/local/go" ]]; then
+    echo "Golang is not installed"
+
+    prompt "Download Golang install binary" "wget https://storage.googleapis.com/golang/go${go_version}.linux-amd64.tar.gz -O $WORK_DIR/go.tar.gz"
+
+    if [ ! sha256sum -c <<< "${go_hash} ${WORK_DIR}/go.tar.gz" ]; then
+        echo "Hash does not match. Aborting!"
+        exit 1
+    else
+        echo "Hash match. Installing Golang"
+        prompt "Install Golang" "tar -c /usr/local -xzf $WORK_DIR/go.tar.gz"
+    fi
+else
+    echo "Golang is already installed"
+fi
 
 # setup ssh 
 echo "Now setup SSH keys so we can clone our Git repos"
-if [ -d "$HOME/.ssh" ]; then
+if [[ -d "$HOME/.ssh" ]]; then
     echo "~/.ssh exists - using keys that are there"
     eval "$(ssh-agent -s)"
     prompt "Add ssh keys" "ssh-add ~/.ssh/id_rsa"
@@ -101,18 +161,17 @@ fi
 
 # setup system_setup repo and install dotfiles
 echo "Now we'll setup git and clone the dotfiles repository"
-prompt "Install git" "brew install git"
 
-if [ -d "$HOME/Documents/system_setup)" ]; then
+if [[ -d "$HOME/Documents/system_setup)" ]]; then
     echo "System setup repo already exits"
-    prompt "Update the repo" "(cd $HOME/Documents/system_setup &&  git submodule init && git submodule update --recursive --remote)"
-    prompt "Install the dotfiles" "(cd $HOME/Documents/system_setup/dotfiles && ./install mac)"
+    prompt "Update the repo" "(cd $HOME/Documents/system_setup && git submodule init && git submodule update --recursive --remote)"
+    prompt "Install the dotfiles" "(cd $HOME/Documents/system_setup/dotfiles && ./install linux)"
 else
     echo "System setup repo does not exist"
 
     prompt "Clone system_setup repo" "git clone git@github.com:skulumani/system_setup.git $HOME/Documents/system_setup"
     echo "Now we'll update the submodules and install all the dotfiles"
-    prompt "Install dotfiles" "(cd $HOME/Documents/system_setup &&  git submodule init && git submodule update --recursive --remote && ./dotfiles/install mac)"
+    prompt "Install dotfiles" "(cd $HOME/Documents/system_setup && git submodule init && git submodule update --recursive --remote && ./dotfiles/install linux)"
 fi
 
 # make sure Anaconda is on the path
@@ -122,21 +181,20 @@ source ~/.profile
 source ~/.bashrc
 python --version
 
-# install vim with python support
-prompt "Install vim" "brew install vim --with-client-server --with-override-system-vi --with-python3"
-
 # install drive client
 echo "Installing Google Drive client"
-go get -u github.com/odeke-em/drive/cmd/drive
+prompt "Install drive client" "go get -u github.com/odeke-em/drive/cmd/drive"
+prompt "Test drive client" "drive --version"
 
 echo "Install all the pip packages"
 prompt "Install pip packages" "install_pips"
 
 # setup zsh as default
-echo "Now set zsh as the default shell"
 prompt "Set zsh as default shell" "chsh -s $(which zsh)"
 
+prompt "Install oh-my-zsh" "sh -c '$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)'"
 echo "All finished"
+exit
 # install brews
 
 # prompt "Update ruby"
