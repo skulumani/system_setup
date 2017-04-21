edaf2bb4a8ef43dcf7de798bcda4adfbe04f2b19
Shankar Kulumani <shanks.k@gmail.com>
2017-04-21 10:20:08 -0400

    Fix texlive install directories

--- linux_setup.sh
+++ linux_setup.sh
@@ -132,11 +132,10 @@ fi
 if [[ ! -d "/usr/local/texlive" ]]; then
     echo "TeXlive is not installed"
     prompt "Download texlive installer" "wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz -O $WORK_DIR/install-tl.tar.gz"
-    eval "mkdir $WORK_DIR/texlive"
-    eval "tar -C $WORK_DIR/texlive -xzf $WORK_DIR/install-tl.tar.gz"
+    eval "tar -C $WORK_DIR -xzf $WORK_DIR/install-tl.tar.gz"
     
     prompt "Create TeXLive directory and set correct permissions" "sudo mkdir /usr/local/texlive/$texlive_year; sudo chown -R $USER: /usr/local/texlive/; sudo chmod -R u+rw /usr/local/texlive"
-    prompt "Install TeXlive" "$WORK_DIR/texlive/install-tl"
+    prompt "Install TeXlive" "$WORK_DIR/install-tl-*/install-tl"
 else
     echo "TexLive is already installed"
 fi
