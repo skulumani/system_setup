68ee5e37cff10f0068ee1c541e16dbc36f79e63e
Shankar Kulumani <shanks.k@gmail.com>
2017-06-02 15:22:57 -0400

    Install tmux script is working

--- build_tmux.sh
+++ build_tmux.sh
@@ -2,14 +2,20 @@
 
 set -e
 
+TMUX_VERSION="2.5"
 sudo apt-get remove --purge tmux 
-sudo apt-get install cmake libevent-dev libncurses5-dev
+sudo apt-get install cmake libevent-dev libncurses5-dev checkinstall
 
-if [[ ! -d "$HOME/vim" ]]; then
-    git clone https://github.com/tmux/tmux.git 
+if [[ ! -d "$HOME/tmux" ]]; then
+    git clone https://github.com/tmux/tmux.git ~/tmux
+    cd ~/tmux
+    git checkout $TMUX_VERSION
+else
+    cd ~/tmux
+    git checkout $TMUX_VERSION
 fi
 
 cd ~/tmux
 sh autogen.sh
 ./configure && make
-sudo make install
+sudo checkinstall
