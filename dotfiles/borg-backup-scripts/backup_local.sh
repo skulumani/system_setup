aeb3998f02d4cd5b7bde81f7ba41432ab9254f1a
Shankar Kulumani <shanks.k@gmail.com>
2018-01-18 22:50:21 -0500

    Modify borg backup wrapper script

--- dotfiles/borg-backup-scripts/backup_local.sh
+++ dotfiles/borg-backup-scripts/backup_local.sh
@@ -43,7 +43,7 @@ readonly COMPRESSION_LEVEL=6
 readonly HOME=/home/shankar
 
 # Whitespace-separated list of paths to back up.
-readonly SOURCE_PATHS="${HOME}/Documents ${HOME}/Downloads ${HOME}/Drive"
+readonly SOURCE_PATHS="${HOME}/Documents ${HOME}/Downloads ${HOME}/Drive/docs ${HOME}/Drive/GWU"
 
 # Whitespace-separated list of paths to exclude from backup.
 readonly EXCLUDE="*.pyc"
