cfe5f31c0b594b7924190d56849f432eaa2374cf
Shankar Kulumani <shanks.k@gmail.com>
2018-05-20 11:40:56 -0400

    Update build scirpt for ag

--- build_scripts/build_ag.sh
+++ build_scripts/build_ag.sh
@@ -1,21 +1,46 @@
 #!/bin/bash
 
-set -e
 AG_VERSION=2.1.0
+WORK_DIR=$(mktemp -d)
+
+# make sure tmp dir was actually created
+if [[ ! -d "$WORK_DIR" ]]; then
+    echo "Could not create temp directory"
+    exit 1
+fi
+
+# delete temp dir
+cleanup () {
+    rm -rf "$WORK_DIR"
+    echo "Deleted temp working directory: $WORK_DIR"
+}
+
+set +e
+options=("Yes" "No" "Quit")
+prompt () {
+    echo "Are you sure you want to $1"
+    select yn in "${options[@]}"; do
+        case $yn in
+            Yes ) eval "$2";
+                break;;
+            No ) break;;
+            Quit ) exit;;
+        esac
+    done
+}
+
+# register cleanup function to be called on exit
+trap cleanup EXIT
+
+sudo apt-get -qq update
 sudo apt-get remove --purge silversearcher-ag
 sudo apt-get install -y automake pkg-config libpcre3-dev zlib1g-dev liblzma-dev checkinstall
 
 echo "We're going to install the silversearcher-ag from source"
 
-if [[ ! -d "$HOME/ag" ]]; then
-    git clone https://github.com/ggreer/the_silver_searcher.git ~/ag
-    cd ~/ag
-    git checkout $AG_VERSION
-else
-    cd ~/ag
-    git checkout $AG_VERSION
-fi
+git clone https://github.com/ggreer/the_silver_searcher.git ${WORK_DIR}/ag
+cd ${WORK_DIR}/ag
+git checkout $AG_VERSION
 
-cd ~/ag
 bash ./build.sh
 sudo checkinstall
