fcb444c73c025e7648c4a2ec3b7751f50f615ca3
Shankar Kulumani <shanks.k@gmail.com>
2018-05-20 14:15:00 -0400

    Add neovim install script

--- build_scripts/build_neovim.sh
+++ build_scripts/build_neovim.sh
@@ -1,38 +1,51 @@
 #!/bin/bash
-set -e
 # Build neovim
-NEOVIM_VERSION="v0.2.0"
-# install prereqs
-sudo apt-get update
-sudo apt-get install libtool libtool-bin autoconf automake cmake \
-    g++ pkg-config unzip libc6-dev libiconv-hook1 libiconv-hook-dev
-# uninstall neovim if installed
-sudo apt-get remove --purge neovim
+NEOVIM_VERSION="nightly"
+WORK_DIR=$(mktemp -d)
 
-# check if neovim already exists
-if [[ -d "$HOME/neovim" ]]; then
-    rm -rIv $HOME/neovim
+# make sure tmp dir was actually created
+if [[ ! -d "$WORK_DIR" ]]; then
+    echo "Could not create temp directory"
+    exit 1
 fi
+
+# delete temp dir
+cleanup () {
+    rm -rf "$WORK_DIR"
+    echo "Deleted temp working directory: $WORK_DIR"
+}
+
+set +e
+options=("Yes" "No" "Quit")
+prompt () {
+    echo "Are you sure you want to $1"
+    select yn in "${options[@]}"; do
+        case $yn in
+            Yes ) eval "$2";
+                break;;
+            No ) break;;
+            Quit ) exit;;
+        esac
+    done
+}
+
+# register cleanup function to be called on exit
+# trap cleanup EXIT
+# install prereqs
+prompt "Run apt-get update?" "sudo apt-get -qq update"
+prompt "Install dependencies?" "sudo apt-get install ninja-build libtool libtool-bin autoconf automake cmake g++ pkg-config unzip texinfo"
+
+# uninstall neovim if installed
+prompt "Uninstall neovim?" "sudo apt-get remove --purge neovim && sudo rm /usr/local/bin/nvim && sudo rm -r /usr/local/share/nvim"
 
 # clone neovim
-if [[ ! -d "$HOME/neovim_source" ]]; then
-    git clone https://github.com/neovim/neovim.git ~/neovim_source
-    cd ~/neovim_source
-    git checkout $NEOVIM_VERSION
-else
-    cd ~/neovim_source
-    git checkout $NEOVIM_VERSION
-fi
+prompt "Clone neovim and install?" "git clone https://github.com/neovim/neovim.git $WORK_DIR/neovim"
+cd $WORK_DIR/neovim
+git checkout $NEOVIM_VERSION
 
-cd ~/neovim_source
-# build
-if [[ -d "build" ]]; then
-    rm -rIv build
-fi
+make 
+sudo make install
 
-make CMAKE_BUILD_TYPE=Release \
-    CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/neovim"
-make install
 
 
 
