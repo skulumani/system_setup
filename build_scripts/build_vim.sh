0d73f64fbe05c89b1d5d57845bea2f5cf0114c32
Shankar Kulumani <shanks.k@gmail.com>
2018-05-20 15:31:44 -0400

    Update vim build  script

--- build_scripts/build_vim.sh
+++ build_scripts/build_vim.sh
@@ -1,8 +1,35 @@
 #!/bin/bash
 
-set -e
+VIM_VERSION="v8.1.0010"
+WORK_DIR=$(mktemp -d)
 
-VIM_VERSION="v8.0.0606"
+# make sure tmp dir was actually created
+if [[ ! -d "$WORK_DIR" ]]; then
+    echo "Could not create temp directory"
+    exit 1
+fi
+
+# delete temp dir
+cleanup () {
+    rm -rf "$WORK_DIR"
+    echo "Deleted temp working directory: $WORK_DIR"
+}
+
+set +e
+options=("Yes" "No" "Quit")
+prompt () {
+    echo "Are you sure you want to $1"
+    select yn in "${options[@]}"; do
+        case $yn in
+            Yes ) eval "$2";
+                break;;
+            No ) break;;
+            Quit ) exit;;
+        esac
+    done
+}
+
+trap cleanup EXIT
 
 sudo apt-get remove --purge vim \
     vim-gtk \
@@ -28,43 +55,52 @@ sudo apt-get install liblua5.1-dev \
     libxpm-dev \
     libxt-dev \
     python3-dev \
+    ruby \
     ruby-dev \
+    libruby \
     libperl-dev \
     git \
     checkinstall
 
 echo "We're going to clone and checkout Vim from github"
 
-if [[ ! -d "$HOME/vim" ]]; then
-    git clone https://github.com/vim/vim.git ~/vim
-else
-    cd ~/vim
-    git checkout $VIM_VERSION
-fi
+git clone https://github.com/vim/vim.git ${WORK_DIR}/vim
+cd ${WORK_DIR}/vim
+git checkout $VIM_VERSION
 
-cd ~/vim
+if [[ ! -d "$HOME/anaconda3/envs/neovim3" ]]; then
+    echo "Install neovim3 environment using conda"
+    exit 1
+fi
 
 if [[ ! -d "$HOME/anaconda3/envs/neovim2" ]]; then
-    echo "Install neovim2 environment using conda"
-else
-    ./configure --with-features=huge \
-                --enable-multibyte \
-                --enable-largefile \
-                --enable-netbeans \
-                --enable-pythoninterp=yes \
-                --with-python-config-dir=$HOME/anaconda3/envs/neovim2/lib/python2.7/config \
-                --enable-gui=auto \
-                --enable-fail-if-missing \
-                --enable-cscope \
-                --enable-pearlinterp=yes \
-                --enable-luainterp=yes \
-                --prefix=/usr 2>config_err.log >config.log || cat config_err.log
+    echo "install neovim2 environemtn using conda"
+    exit 1
 fi
-make VIMRUNTIMEDIR=/usr/share/vim/vim80
 
-cd ~/vim
+./configure --with-features=huge \
+            --enable-multibyte \
+            --enable-pearlinterp=yes \
+            --enable-luainterp=yes \
+            --enable-pythoninterp=yes \
+            --with-python-config-dir=$HOME/anaconda3/envs/neovim2/lib/python2.7/config \
+            --enable-python3interp \
+            --with-python3-config-dir=$HOME/anaconda3/envs/neovim3/lib/python3.6/config-3.6m-x86_64-linux-gnu \
+            --with-luajit \
+            --with-x \
+            --enable-gui=auto \
+            --enable-fail-if-missing \
+            --enable-cscope \
+            --enable-largefile \
+            --enable-netbeans \
+            --enable-fontset \
+            --with-compiledby="Shankar Kulumani" \
+            --prefix=/usr/local
+
+make
 sudo checkinstall
-# sudo make install
+
+# # sudo make install
 sudo update-alternatives --install /usr/bin/editor editor /usr/bin/vim 1
 sudo update-alternatives --set editor /usr/bin/vim
 sudo update-alternatives --install /usr/bin/vi vi /usr/bin/vim 1
