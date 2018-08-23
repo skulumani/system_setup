d96b51d9b29e99e9e31285ac5cc17b54011ba131
Shankar Kulumani <shanks.k@gmail.com>
2018-08-23 09:44:00 -0400

    Fix oracle install command

--- build_scripts/setup_jabref.sh
+++ build_scripts/setup_jabref.sh
@@ -24,7 +24,8 @@ fi
 echo "Install Oracle  JAVA"
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
-suod apt-get install oracle-java8-installer
+sudo apt-get install oracle-java8-installer
+
 # setup links for Jabref icon/menu item
 
 # should be setup by the dotfiles installer
