435cfe47cc7216e5ed923832115acd130b7f4059
Shankar Kulumani <shanks.k@gmail.com>
2019-01-25 11:55:46 -0500

    Update rpi setup

--- rpi_setup.sh
+++ rpi_setup.sh
@@ -1,15 +1,21 @@
 sudo apt-get update
 sudo apt-get install vim git default-jre
 sudo apt-get install libunixsocket-java # for Signal dbus 
+sudo apt-get install python3 python3-dev
 
 # basic git setup
 git config --global user.name "Shankar - RPi"
 git config --global user.email shanks.k@gmail.com
+
+# setup ssh keys
+ssh-keygen -t rsa -b 4096 -C "shanks.k@gmail.com"
+ssh-add ~/.ssh/id_rsa
 
 # download signal and add to path
 
 # download no-ip dynamic update client
 
 # download Pi VPN
+
 
 
