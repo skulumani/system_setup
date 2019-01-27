dfa9676d991a925521f0d9538de32d8d8054e305
Shankar Kulumani <shanks.k@gmail.com>
2019-01-27 12:48:54 -0500

    Use dbus for borg cron

--- dotfiles/borg-backup-scripts/borg_local_cron.sh
+++ dotfiles/borg-backup-scripts/borg_local_cron.sh
@@ -35,7 +35,7 @@ if [ ! -d "${REPO}" ]; then
     /usr/bin/borg init --encryption=repokey "${REPO}"
 fi
 
-MSG=$((/usr/bin/borg create --verbose --stats --compression "${COMPRESSION_ALGO},${COMPRESSION_LEVEL}" \
+BORG_MESSAGE=$((/usr/bin/borg create --verbose --stats --compression "${COMPRESSION_ALGO},${COMPRESSION_LEVEL}" \
     --exclude "$EXCLUDE" \
     "${REPO}::{hostname}-{now:%Y-%m-%dT%H:%M:%S}" $SOURCE_PATHS) 2>&1)
 
@@ -47,12 +47,15 @@ MSG=$((/usr/bin/borg create --verbose --stats --compression "${COMPRESSION_ALGO}
 #         --prefix '{hostname}-' --verbose --stats \
 #         "${REPO}"
 
-echo "SUCCESS $(date +%Y-%m-%dT%H:%M:%S)${newline}${HOSTNAME} ${MSG}" | /opt/signal-cli/bin/signal-cli --dbus-system send "+16303366257"
+SIGNAL_MESSAGE="SUCCESS $(date +%Y-%m-%dT%H:%M:%S) ${HOSTNAME}${newline}${BORG_MESSAGE}"
+# echo "SUCCESS $(date +%Y-%m-%dT%H:%M:%S)${newline}${HOSTNAME} ${MSG}" | /opt/signal-cli/bin/signal-cli --dbus-system send "+16303366257"
 
 } || {
 
-echo "FAILURE $(date +%Y-%m-%dT%H:%M:%S)${newline}${HOSTNAME} ${MSG}" | /opt/signal-cli/bin/signal-cli --dbus-system send "+16303366257"
+SIGNAL_MESSAGE="FAILURE $(date +%Y-%m-%dT%H:%M:%S) ${HOSTNAME}${newline}${BORG_MESSAGE}"
+# echo "FAILURE $(date +%Y-%m-%dT%H:%M:%S)${newline}${HOSTNAME} ${MSG}" | /opt/signal-cli/bin/signal-cli --dbus-system send "+16303366257"
 }
 
+dbus-send --system --type=method_call --print-reply --dest="org.asamk.Signal" /org/asamk/Signal org.asamk.Signal.sendMessage string:"${SIGNAL_MESSAGE}" array:string: string:"+16303366257"
  
 
