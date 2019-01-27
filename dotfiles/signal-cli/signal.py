be943d13d2a4db7237044a1c8eef09989a4a89e2
Shankar Kulumani <shanks.k@gmail.com>
2019-01-27 16:40:50 -0500

    Update signal-cli python

--- dotfiles/signal-cli/signal.py
+++ dotfiles/signal-cli/signal.py
@@ -4,27 +4,32 @@
 from pydbus import SystemBus
 from gi.repository import GLib
 import argparse
+from datetime import datetime
 
-def send_message():
-    pass
+def send_message(number, message):
+    bus = SystemBus()
+    signal = bus.get('org.asamk.Signal')
+    signal.sendMessage(message, [], number)
+    return 0
 
-def receive_message(timestamp, source, groupID, message, attachments):
-    print ("msgRcv called")
-    print (message)
-    return
+def msgRcv(timestamp, source, groupID, message, attachments):
+    # groupID is empty if not a group
+    # attachments will be a path to the attachment
 
-bus = SystemBus()
-loop = GLib.MainLoop()
+    # convert unix time to something useful
+    # ts = datetime.utcfromtimestamp(int(timestamp)).strftime('%Y-%m-%d %H:%M:%S')
+    print("\n{} From:{}\n{}".format(timestamp, source, message))
+    # parse the message to check for strings
+    return 0
 
-signal = bus.get('org.asamk.Signal')
+def receive_message_loop():
+    bus = SystemBus()
+    loop = GLib.MainLoop()
+    signal = bus.get('org.asamk.Signal')
+    signal.onMessageReceived = msgRcv
+    loop.run()
 
-signal.onMessageReceived = msgRcv
-loop.run()
-
-signal.sendMessage(string, [], ['number with country code'])
-# replace ___USER___ with the corresponding phone number
-
-signal.sendGroupMessage(data, [], [___GROUP___])
+# signal.sendGroupMessage(data, [], [___GROUP___])
 # replace ___GROUP___ with byte-representation of 
 # e.g. 0x01, 0x23,  0x45, 0x67,  0x89, 0xab,   0xcd, 0xef,   0x01, 0x23,   0x45, 0x67,   0x89, 0xab,   0xcd, 0xef
 # obtain these bytes by:
@@ -35,4 +40,16 @@ signal.sendGroupMessage(data, [], [___GROUP___])
 # and splitting the two-byte words into single bytes
 
 if __name__ == "__main__":
-    pass
+    parser = argparse.ArgumentParser(description="Send/Receive Signal messages")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('--send', nargs=2, help="Send a message to number")
+    group.add_argument('--receive', action='store_true', help='Receive messages and print')
+    args = parser.parse_args()
+
+    if args.send:
+        number = args.send[1:]
+        message = args.send[0]
+        send_message(number, message)
+
+    elif args.receive:
+        receive_message_loop()
