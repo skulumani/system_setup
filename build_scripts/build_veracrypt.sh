4867dbbcdad20f99067405137cacc9555b3c4bf3
Shankar Kulumani <shanks.k@gmail.com>
2019-04-15 15:58:28 -0400

    Update build veracrypt script

--- build_scripts/build_veracrypt.sh
+++ build_scripts/build_veracrypt.sh
@@ -2,7 +2,7 @@
 
 # Links from https://www.veracrypt.fr/en/Downloads.html
 # Instrucitons - https://www.veracrypt.fr/en/Digital%20Signatures.html
-
+# SO answer https://askubuntu.com/questions/929195/what-is-the-recommended-way-to-use-veracrypt-in-ubuntu
 VERSION="1.23"
 
 GPG_FINGERPRINT="5069A233D55A0EEB174A5FC3821ACD02680D16DE"
@@ -12,15 +12,53 @@ SETUP_URL="https://launchpad.net/veracrypt/trunk/${VERSION}/+download/veracrypt-
 SIG_URL="https://launchpad.net/veracrypt/trunk/${VERSION}/+download/veracrypt-${VERSION}-setup.tar.bz2.sig"
 SUMS_URL="https://launchpad.net/veracrypt/trunk/${VERSION}/+download/veracrypt-${VERSION}-sha256sum.txt"
 
+WORK_DIR=$(mktemp -d )
+
+# delete temp dir
+cleanup () {
+    rm -rf "$WORK_DIR"
+    echo "Deleted temp working directory: $WORK_DIR"
+}
+
+# register cleanup function to be called on exit
+trap cleanup EXIT
+
+command_exists () {
+    command -v "$1" >/dev/null 2>&1 ;
+}
+
+echo "This will download and verify Veracrypt"
+
+echo "Work Dir: $WORK_DIR"
+
 # download the veracrypt gpg key
+gpg --keyserver hkp://keyserver.ubuntu.com --recv-keys $GPG_PUBID
 
 # verify the fingerprint
+echo "Now we need to verify the fingerprint"
 
+gpg --fingerprint $GPG_PUBID 
+
+echo "Desired fingerprint: $GPG_FINGERPRINT"
+read -p "Press enter when matching"
 # download the setup file, signature, and shasums
 
+echo "Now we'll download"
+read -p "Press Enter to download"
+
+wget $SETUP_URL -P $WORK_DIR
+wget $SIG_URL -P $WORK_DIR
+wget $SUMS_URL -P $WORK_DIR
+
 # verify the files
+cd $WORK_DIR
+gpg --verify "veracrypt-$VERSION-sha256sum.txt.sig" "veracrypt-$VERSION-sha256sum.txt"
+gpg --verify "veracrypt-$VERSION-setup.tar.bz2.sig" "veracrypt-$VERSION-setup.tar.bz2"
+
+read -p "Press enter if valid verification"
 
 # extract and install
 
-tar xvf veracrypt.tar.bz2
-./veracrypt*
+tar xvf "veracrypt-$VERSION-setup.tar.bz2"
+read -p "Press enter to start installation"
+eval "./veracrypt-$VERSION-setup-gui-x64"
