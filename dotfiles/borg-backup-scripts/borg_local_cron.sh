b2d18ff6b1ce7cdef148a3a0463278b0d9ed9aa3
Shankar Kulumani <shanks.k@gmail.com>
2019-04-15 10:59:45 -0400

    Modify borg local script

--- dotfiles/borg-backup-scripts/borg_local_cron.sh
+++ dotfiles/borg-backup-scripts/borg_local_cron.sh
@@ -35,7 +35,7 @@ if [ ! -d "${REPO}" ]; then
     /usr/bin/borg init --encryption=repokey "${REPO}"
 fi
 
-BORG_MESSAGE=$((/usr/bin/borg create --verbose --stats --compression "${COMPRESSION_ALGO},${COMPRESSION_LEVEL}" \
+BORG_MESSAGE=$((/home/shankar/bin/borg create --verbose --stats --compression "${COMPRESSION_ALGO},${COMPRESSION_LEVEL}" \
     --exclude "$EXCLUDE" \
     "${REPO}::{hostname}-{now:%Y-%m-%dT%H:%M:%S}" $SOURCE_PATHS) 2>&1)
 
@@ -56,6 +56,6 @@ SIGNAL_MESSAGE="FAILURE $(date +%Y-%m-%dT%H:%M:%S) ${HOSTNAME}${newline}${BORG_M
 # echo "FAILURE $(date +%Y-%m-%dT%H:%M:%S)${newline}${HOSTNAME} ${MSG}" | /opt/signal-cli/bin/signal-cli --dbus-system send "+16303366257"
 }
 
-dbus-send --system --type=method_call --print-reply --dest="org.asamk.Signal" /org/asamk/Signal org.asamk.Signal.sendMessage string:"${SIGNAL_MESSAGE}" array:string: string:"+16303366257"
+# dbus-send --system --type=method_call --print-reply --dest="org.asamk.Signal" /org/asamk/Signal org.asamk.Signal.sendMessage string:"${SIGNAL_MESSAGE}" array:string: string:"+16303366257"
  
 
