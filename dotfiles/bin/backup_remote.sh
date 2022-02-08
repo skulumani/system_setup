77f88933b6ea083e07b466630e3833eb67c0d6a9
Shankar Kulumani <shanks.k@gmail.com>
2022-02-07 21:15:53 -0500

    Add another path for backup to borg

--- dotfiles/bin/backup_remote.sh
+++ dotfiles/bin/backup_remote.sh
@@ -48,6 +48,7 @@ readonly COMPRESSION_LEVEL=6
 export SOURCE_PATHS="${HOME}/Documents ${HOME}/Downloads"
 [ -d "${HOME}/Drive" ] && SOURCE_PATHS+=" ${HOME}/Drive"
 [ -d "/media/shankar/data/timelapse" ] && SOURCE_PATHS+=" /media/shankar/data/timelapse"
+[ -d "/media/shankar/data/syncthing/docs" ] && SOURCE_PATHS+=" /media/shankar/data/syncthing/docs"
 
 # Whitespace-separated list of paths to exclude from backup.
 readonly EXCLUDE="*.pyc"
