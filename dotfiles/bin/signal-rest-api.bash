fb38aef480bd396e08bc89aaf6a630d6f1376f14
Shankar Kulumani <shanks.k@gmail.com>
2022-03-11 22:44:39 -0500

    Update system setup

---
+++ dotfiles/bin/signal-rest-api.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+# API to talk to Signal rest api client
+# From: https://github.com/florian-h05/linux_openhab-misc/blob/main/signal-cli-rest-api_client.bash
+
+
+host="192.168.88.12:8080"
